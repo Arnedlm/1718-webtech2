5eeac72c918dd09e0be36ac8ea43d320a4d52c0d
Arnedlm <arne.delamarche@outlook.com>
2018-05-24 13:16:45 +0200

    update

---
+++ week10/oef1/index.php
@@ -0,0 +1,6 @@
+<?php
+
+$liv = "<h1>Liverpool ";
+$vs = "vs. ";
+$rm = "Real Madrid</h1>";
+echo $liv.$vs.$rm;
---
+++ week10/vb1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Voorbeeld 1</title>
+</head>
+<body>
+    <h1>Voorbeeld 1</h1>
+    <?php 
+    
+    $int = 1; $float = 1.1; $string = "hello"; $boolean = true; $array = [1, 2, 3];
+
+    var_dump($int);
+    var_dump($float);
+    var_dump($string);
+    var_dump($boolean);
+    var_dump($array);
+    
+    ?>
+</body>
+</html>
